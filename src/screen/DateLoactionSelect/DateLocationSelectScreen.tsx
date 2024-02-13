94fb52ae02c8f28f190d67c22215f39e33d31e16
Lyheng <senglyheng2001@gmail.com>
2024-02-13 18:16:13 +0700

    first-commit

--- src/screen/DateLoactionSelect/DateLocationSelectScreen.tsx
+++ src/screen/DateLoactionSelect/DateLocationSelectScreen.tsx
@@ -5,33 +5,33 @@ import CreatableSelect from 'react-select/creatable';
 import axios from 'axios';
 
 const DateLocationSelectScreen = () => {
-  const [title, setTitle] = useState(true)
-  const [gif, setGif] = useState("https://media4.giphy.com/media/CJ5bKVKLSQsrs3nJw2/giphy.gif?cid=ecf05e47rly5v8gd0bphd5ct7n85siobd7eyy0xe3hkml64d&ep=v1_gifs_related&rid=giphy.gif&ct=s")
+//   const [title, setTitle] = useState(true)
+//   const [gif, setGif] = useState("https://media4.giphy.com/media/CJ5bKVKLSQsrs3nJw2/giphy.gif?cid=ecf05e47rly5v8gd0bphd5ct7n85siobd7eyy0xe3hkml64d&ep=v1_gifs_related&rid=giphy.gif&ct=s")
 
 
-  const options = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' }
-  ]
-  const [selectedValues, setSelectedValues] = useState([]);
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    setGif(
-      'https://media3.giphy.com/media/jyuuoHidVXPuJ2iBRT/giphy.gif?cid=ecf05e470ggvsfgcfvaku3680ddxs6r8epgs7adgdpeapi89&ep=v1_gifs_related&rid=giphy.gif&ct=s'
-    );
-    axios.post(
-      `https://api.telegram.org/bot${'6426676306:AAEhCk0qYiYAy2TWteD4CUHq9TvfGbcZtdA'}/sendMessage`,
-      {
-        chat_id: 898391639,
-        text: `Location: ${selectedValues?.value}`,
-      }
-    );
-  };
+//   const options = [
+//     { value: 'chocolate', label: 'Chocolate' },
+//     { value: 'strawberry', label: 'Strawberry' },
+//     { value: 'vanilla', label: 'Vanilla' }
+//   ]
+//   const [selectedValues, setSelectedValues] = useState([]);
+//   const handleSubmit = (e: any) => {
+//     e.preventDefault();
+//     setGif(
+//       'https://media3.giphy.com/media/jyuuoHidVXPuJ2iBRT/giphy.gif?cid=ecf05e470ggvsfgcfvaku3680ddxs6r8epgs7adgdpeapi89&ep=v1_gifs_related&rid=giphy.gif&ct=s'
+//     );
+//     axios.post(
+//       `https://api.telegram.org/bot${'6426676306:AAEhCk0qYiYAy2TWteD4CUHq9TvfGbcZtdA'}/sendMessage`,
+//       {
+//         chat_id: 898391639,
+//         text: `Location: ${selectedValues?.value}`,
+//       }
+//     );
+//   };
 
   return (
     <div className="wrapper">
-      <h2 className="question">{
+      {/* <h2 className="question">{
         title ? `${"Do we date today?"}` : `${"I love you too!❤️"}`
       }</h2>
       <div className="gif">
@@ -52,7 +52,7 @@ const DateLocationSelectScreen = () => {
           <button type="submit">Submit</button>
         </form>
 
-      </div>
+      </div> */}
 
     </div>
   )
